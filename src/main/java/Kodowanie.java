93b229a7d6aaab6dd8d602e7da0bf8a898556627
242425 <242425@edu.p.lodz.pl>
2023-03-17 17:27:15 +0100

    Klasy

---
+++ src/main/java/Kodowanie.java
@@ -0,0 +1,2 @@
+public class Kodowanie {
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+
+public class Main {
+    public static void main(final String[] args) {
+        System.out.println("Program na Zadanie 1");
+
+    }
+}
